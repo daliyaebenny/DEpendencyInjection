eafebad98dd15d9cfaf202c3f232e7f0813c6d67
Daliya Benny <daliyaebenny@gmail.com>
2022-06-24 10:02:16 -0700

    Configured Spring.xml,added getter and setters in all the classes for XML based setter injection

--- src/main/java/org/genspark/Phone.java
+++ src/main/java/org/genspark/Phone.java
@@ -3,4 +3,12 @@ package org.genspark;
 public class Phone {
 
     private String mob;
+
+    public String getMob() {
+        return mob;
+    }
+
+    public void setMob(String mob) {
+        this.mob = mob;
+    }
 }
